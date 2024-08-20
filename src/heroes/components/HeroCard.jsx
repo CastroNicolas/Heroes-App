46a8ccd2fad95fccb02b4ddce12ee96199b189af
CastroNicolas <maximonicolascastro@gmail.com>
2024-08-20 16:54:39 +0200

    text pictures

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -12,7 +12,7 @@ export const HeroCard = ({
     first_appearance,
     characters
 }) => {
-    const heroImageUrl = `https://castronicolas.github.io/Heroes-App/assets/heroes/${id}.jpg`;
+    const heroImageUrl = `https://castronicolas.github.io/Heroes-App/heroes/${id}.jpg`;
 
     return (
         <div className="col">
